10f19cfb9cb2cd6b73d777dfd687a0c54540aca2
Sydney <turnbull.sydney@gmail.com>
2017-11-18 15:46:55 -0500

    got the progress bar working and fixed another calculating bug

--- app/src/main/java/com/example/sydney/vacationcalculator/MainActivity.java
+++ app/src/main/java/com/example/sydney/vacationcalculator/MainActivity.java
@@ -9,6 +9,7 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import static java.lang.Float.parseFloat;
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     EditText startingValue;
     EditText dailyAccumulation;
     float collectedHours;
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,33 +38,42 @@ public class MainActivity extends AppCompatActivity {
         eightHours.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 collectedHours -= 8;
+                //store new collected value
                 SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPref.edit();
                 editor.putFloat("currentValue", collectedHours);
                 editor.commit();
+                //update current accumualation and corresponding progress chart
                 showCurrentHoursCollected.setText(Float.toString(collectedHours));
+                progressBar.setProgress((int)Math.floor(collectedHours));
             }
         });
 
         fourHours.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 collectedHours -= 4;
+                //store new collected value
                 SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPref.edit();
                 editor.putFloat("currentValue", collectedHours);
                 editor.commit();
+                //update current accumualation and corresponding progress chart
                 showCurrentHoursCollected.setText(Float.toString(collectedHours));
+                progressBar.setProgress((int)Math.floor(collectedHours));
             }
         });
 
         oneHour.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 collectedHours -= 1;
+                //store new collected value
                 SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPref.edit();
                 editor.putFloat("currentValue", collectedHours);
                 editor.commit();
+                //update current accumualation and corresponding progress chart
                 showCurrentHoursCollected.setText(Float.toString(collectedHours));
+                progressBar.setProgress((int)Math.floor(collectedHours));
             }
         });
 
@@ -73,19 +84,30 @@ public class MainActivity extends AppCompatActivity {
                 //update values based on user input
                 SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPref.edit();
+                //update values stroed in shared preferences
                 editor.putFloat("dailyAccumulated", dailyAccumulatedValue);
                 editor.putFloat("currentValue", startingHoursValue);
+                //update collected text
                 showCurrentHoursCollected.setText(Float.toString(startingHoursValue));
+                //update collected to be starting
                 collectedHours = startingHoursValue;
+                //update progress bar
+                progressBar.setProgress((int)Math.floor(collectedHours));
                 editor.commit();
             }
         });
 
+        //get last value and input it where appropriate
         SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
         float currentValue = sharedPref.getFloat("currentValue", 0);
         startingValue.setText(Float.toString(sharedPref.getFloat("currentValue", 0)));
         dailyAccumulation.setText(Float.toString(sharedPref.getFloat("dailyAccumulated", 0)));
+        collectedHours = currentValue;
+        //set progress bar value to stored value
+        progressBar = (ProgressBar)findViewById(R.id.countdownTimer);
+        progressBar.setProgress((int)Math.floor(currentValue));
 
+        //set current hours collected to stored value
         showCurrentHoursCollected.setText(Float.toString(currentValue));
     }
 }
